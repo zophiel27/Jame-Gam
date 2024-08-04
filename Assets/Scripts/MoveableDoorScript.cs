cd117e3421031fc7d30dd156f29d765f1f46c711
Isbah Malik <malikisbah@gmail.com>
2024-08-04 19:05:14 +0500

    funny lil mechanisms on level 4 hehe

---
+++ Assets/Scripts/MoveableDoorScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Timeline;
+using UnityEngine;
+
+public class MoveableDoorScript : MonoBehaviour
+{
+    float move_offset = 0;
+    bool is_active = false;
+    GameObject door;
+    private void Start()
+    {
+        door = transform.Find("Door").gameObject;
+    }
+    void Update()
+    {
+        if (is_active) {
+            float movement = move_offset * Time.deltaTime;
+            door.transform.localPosition = new Vector3(door.transform.localPosition.x, door.transform.localPosition.y + movement, door.transform.localPosition.z);
+            if (door.transform.localPosition.y >= 1 || door.transform.localPosition.y <= -1) {
+                is_active = false;
+            } 
+        }
+    }
+    [ContextMenu("Activate Door")]
+    public void Activate() {
+        if (!is_active)
+        {
+            is_active = true;
+            move_offset = door.transform.localPosition.y >= 1 ? -0.2f : 0.2f;
+        } 
+    }
+}
