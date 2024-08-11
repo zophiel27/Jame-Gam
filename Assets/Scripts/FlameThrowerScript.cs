b5318f78b12be2a5108f9d46b4b7f4e637341b53
Isbah Malik <malikisbah@gmail.com>
2024-08-11 13:23:34 +0500

    lvl 6 bleh bleh bleh

--- Assets/Scripts/FlameThrowerScript.cs
+++ Assets/Scripts/FlameThrowerScript.cs
@@ -6,11 +6,17 @@ public class FlameThrowerScript : MonoBehaviour
 {
     public GameObject Fiyyah;
     public bool is_active = false;
+    private AudioSource audioSource;
+    private void Start()
+    {
+       audioSource = GetComponent<AudioSource>();
+    }
     public void Activate()
     {
         if (!is_active) {
             is_active = true;
             GameObject fiyah = Instantiate(Fiyyah, new Vector3(transform.localPosition.x + 2.3f, transform.localPosition.y - 1.7f, transform.localPosition.z), Quaternion.Euler(0, 0, -90));
+            ToggleSound();
             Destroy(fiyah, 2f);
             StartCoroutine(SetIsActiveFalseAfterDelay(2f));
         }
@@ -19,5 +25,14 @@ public class FlameThrowerScript : MonoBehaviour
     {
         yield return new WaitForSeconds(delay);
         is_active = false;
+        ToggleSound();
     }
+
+    void ToggleSound() {
+        if (audioSource.isPlaying)
+            audioSource.Stop();
+        else
+            audioSource.Play();
+    }
+
 }
